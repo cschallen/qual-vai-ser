ca280cc47a6745d42ff5f46fabed7eb2c7127094
cschallenberger <c.schallenberger@hotmail.com>
2017-05-11 22:07:20 -0300

    subindo pasta com css

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-	echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Qual vai ser?</title>
+		<link rel="stylesheet" href="/stylesheets/home.css">
+	</head>
+	<body>
+		<h1 class="red-text">Hello, World!</h1>
+	</body>
+</html>
