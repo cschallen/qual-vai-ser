19cd565c597229bdd39922a9f449acd6cc6f4d5b
cschallenberger <c.schallenberger@hotmail.com>
2017-05-26 15:15:45 -0300

    adicionar style na home

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html>
 <head>
 	<meta charset="utf-8">
 	<title>Qual vai ser?</title>
 	<link rel="stylesheet" href="/stylesheets/home.css">
+	<link rel="stylesheet" href="/stylesheets/novo_restaurante.css">
 </head>
-<body>
+
+<div class="header">
+
+</div>
+
+<div class="center">
+
 	<h1 class="red-text">Hello, World!</h1>
 	<a href="/novo_restaurante.php">Adicionar novo restaurante</a>
-</body>
-</html>
+</div>
+
+<div class="footer">
+
+</div>
