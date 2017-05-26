b55944edf8402dc95ed13db334413651b96787d8
cschallenberger <c.schallenberger@hotmail.com>
2017-05-26 14:39:12 -0300

    adicionar estilo pra pagina de cadastro

--- novo_restaurante.php
+++ novo_restaurante.php
@@ -1,28 +1,72 @@
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/stylesheets/novo_restaurante.css">
 </head>
 <!--  CRIAR DEPOIS O CARDAPIO JUNTO COM O RESTAURANTE -->
 
+<div class="header">
+</div>
 
-<form action="novo_restaurante.php" method="post">
-  <label for="nome"> Nome: </label>
-  <input type="text" name="nome"><br>
-  <label for="descricao">Descrição: </label>
-  <input type="text" name="descricao" ><br>
-  <label for="rua">Rua: </label>
-  <input type="text" name="rua" ><br>
-  <label for="cep">Cep: </label>
-  <input type="text" name="cep" ><br>
-  <label for="cnpj">Cnpj: </label>
-  <input type="text" name="cnpj" ><br>
-  <button type="submit" name="button">Cadastrar</button>
-</form>
+<div class="center">
+    <div class="container">
+        <h3>Cadastre seu restaurante:</h3>
+        <form action="novo_restaurante.php" method="post" class="form">
+            <div class="form-div">
+                <div class="form-inputs">
+                    <label for="nome"> Nome: </label>
+                    <input type="text" name="nome" placeholder="Ex: Bar do Zé">
+                </div>
+
+                <div class="form-inputs">
+                    <label for="rua">Endereço: </label>
+                    <input type="text" name="rua" placeholder="Ex: Av. Ipiranga, 6681">
+                </div>
+            </div>
+
+
+            <div class="form-div">
+                <div class="form-inputs">
+                    <label for="cep">Cep: </label>
+                    <input type="text" name="cep" placeholder="Ex: 00.000.000">
+                </div>
+                <div class="form-inputs">
+                    <label for="cnpj">Cnpj: </label>
+                    <input type="text" name="cnpj" placeholder="Ex: 00.000.000/0000-00">
+                </div>
+            </div>
+
+            <div class="form-div-description">
+                <label for="descricao">Descrição: </label>
+                <textarea rows="6" cols="60" name="descricao" placeholder="Digite aqui a descrição do seu restaurante..."></textarea>
+            </div>
+
+            <div class="form-button">
+                <button class="button" type="submit" name="button">Cadastrar</button>
+            </div>
+        </form>
+
+    </div>
+
+
+</div>
+
+
+
+
+<div class="footer">
+
+</div>
+
+
+
+
+
 <?php
 $con = new PDO("pgsql:host=localhost port=5432 dbname=qual-vai-ser user=postgres password=postgres");
 
 if (!$con) {
-  echo "An error occurred.\n";
-  exit;
+    echo "An error occurred.\n";
+    exit;
 }
 ?>
 
