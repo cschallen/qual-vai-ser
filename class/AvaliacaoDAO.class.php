4ed49d96ec045acc3b047a286dfe4f18828fb297
dottix <alu201513113@alunos.uniritter.edu.br>
2017-06-04 14:23:34 -0300

    Update AvaliacaoDAO.class.php

--- class/AvaliacaoDAO.class.php
+++ class/AvaliacaoDAO.class.php
@@ -1,6 +1,8 @@
 <?php
-require "Conexao.class.php";
-require "Avaliacao.class.php";
+include_once "Conexao.class.php";
+include_once "Avaliacao.class.php";
+include_once "EstabelecimentoDAO.class.php";
+
 class AvaliacaoDAO{
     private $con;
 
@@ -8,33 +10,23 @@ class AvaliacaoDAO{
         $this->con = Conexao::obterConexao();
     }
 
-    public function obter($id){   //obtém uma linha da tabela e retorna um obj equivalente. Retorna só um pois id não se repete
-        $notas = array();; //se não encontrar o livro, pelo menos retorna o null
-        $comando = "SELECT  nota
-        FROM nota
-        WHERE idEstabelecimento = $id";
-        foreach ($this->con->query($comando) as $linha){
-            $nota = new Nota($linha['nota']);
-            $notas[] = $nota;
-        }
+    public function obterEstabelecimentos($estabelecimento){  
+        $colecao = array();
+        $comando = "SELECT  idAvaliacao, nota
+        FROM Avaliacao
+        WHERE idEstabelecimento = $estabelecimento";
+        
+        $estabelecimentoDAO = new EstabelecimentoDAO();
 
-        return $notas;
+				foreach ($this->con->query($comando) as $linha){ 
+						$estabObj = $EstabelecimentoDAO->obter($estabelecimento);
 
-    }
+                        $obj = new Avaliacao($nota, $estabelecimento); 
+						$obj->setIdAvaliacao($linha['idavaliacao']);
+						$colecao[] = $obj;
+				}				
+		return $colecao;
 
-    //Insere nova linha ou atualiza linha existente
-    public function salvar($obj){
-        if($obj instanceof Nota){ //se for uma instancia da classe livro, faz tudo que tem que fazer
-            //se nao vem com id é novo, se tem id é pra atualizar
-            $nota = $obj->getNota();
-            $idEstabelecimento = $obj->getIdEstabelecimento;
-
-            if($idEstabelecimento == NULL){ //se o id do estabelecimento retornando vier nulo, faz o insert se nao update
-                $comando = "INSERT INTO Nota(nota) VALUES ('$nota')";
-            }
-
-            return $this->con->exec($comando); //nao pode trocar id, se trocar ele se perde
-        }
     }
 
 }
