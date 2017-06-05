b0e3d453f2f753133ad5d242f871fa4d5a9d7de1
cschallenberger <c.schallenberger@hotmail.com>
2017-06-05 08:58:45 -0300

    arrumar testes

--- cadastro.php
+++ cadastro.php
@@ -29,7 +29,7 @@
         $(document).ready(function () {
 
             $('#cep').mask('00000-000');
-            $('#cnpj').mask('00.000.000/0000-00', {reverse: true});
+            $('#cnpj').mask('00.000.000/0000-00');
         });
     })(jQuery);
 
--- test/class/EstabelecimentoTest.php
+++ test/class/EstabelecimentoTest.php
@@ -7,37 +7,43 @@ require_once './class/Estabelecimento.class.php';
 class EstabelecimentoTeste extends PHPUnit{
 
     public function testRua() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
         $this->assertEquals('Rua X', $estabelecimento->getRua());
     }
 
     public function testDescricao() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals("Descricao do estabelecimento", $estabelecimento->getDescricao());
     }
 
     public function testCep() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals("91450-140", $estabelecimento->getCep());
     }
 
     public function testCNPJ() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals("1948375938", $estabelecimento->getCnpj());
     }
 
     public function testNome() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals("Estabelecimento Teste", $estabelecimento->getNome());
     }
 
     public function testCardapio() {
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals("Cardapio do estabeleicmento", $estabelecimento->getCardapio());
     }
 
     public function testeQtdEstabelecimento(){
-        $estabelecimento = new Estabelecimento("Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Estabelecimento Teste", "Cardapio do estabeleicmento");
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", "1948375938", "Cardapio do estabeleicmento");
+
         $this->assertEquals(1, count($estabelecimento));
     }
 
