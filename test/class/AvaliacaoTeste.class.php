c7d7d9eccc2a3570d8fae3d5da5c8f0e299c2b99
cschallenberger <c.schallenberger@hotmail.com>
2017-06-04 21:56:18 -0300

    arrumar testes da casse avaliacao

--- test/class/AvaliacaoTeste.class.php
+++ test/class/AvaliacaoTeste.class.php
@@ -7,18 +7,18 @@ require_once './class/Avaliacao.class.php';
 class AvaliacaoTest extends PHPUnit{
 
     public function testType() {
-        $avaliacao = new Avaliacao(1, 4, 3);
+        $avaliacao = new Avaliacao(4, 3);
         $this->assertInternalType('int', $avaliacao->getNota());
     }
 
-    public function testIdAvaliacao() {
-        $avaliacao = new Avaliacao(1, 4, 3);
-        $this->assertEquals(1, $avaliacao->getIdAvaliacao());
+    public function testNota() {
+        $avaliacao = new Avaliacao(4, 3);
+        $this->assertEquals(4, $avaliacao->getNota());
     }
 
     public function testIdEstabelecimento() {
-        $avaliacao = new Avaliacao(1, 2, 1);
-        $this->assertEquals(1, $avaliacao->getIdEstabelecimento());
+        $avaliacao = new Avaliacao(2, 1);
+        $this->assertEquals(1, $avaliacao->getEstabelecimento());
     }
 
 }
