0288abf544e95c981bc4f156eb6007914bcd316e
Unknown <tigor.ricardo@hotmail.com>
2017-06-07 22:25:59 -0300

    cardapio em tabela separada

--- db/scrypts/postgres.sql
+++ db/scrypts/postgres.sql
@@ -6,7 +6,6 @@ Create table Estabelecimento
 	cep character varying(15) NOT NULL,
 	cnpj character varying(25) NOT NULL UNIQUE,
 	nome character varying(100) NOT NULL,
-	cardapio character varying(100) NOT NULL,
 
  	CONSTRAINT Estabelecimento_pkey primary key (id_estabelecimento)
 )
@@ -28,13 +27,54 @@ WITH (
   OIDS=FALSE
 );
 
+Create table Cardapio
+(
+	id_cardapio serial NOT NULL,
+	dia integer NOT NULL,
+	id_estabelecimento integer NOT NULL,
+
+ CONSTRAINT Cardapio_pkey primary key (id_cardapio),
+ CONSTRAINT Cardapio_id_estabelecimento_fkey foreign key(id_estabelecimento)
+ REFERENCES Estabelecimento (id_estabelecimento)
+)
+WITH (
+  OIDS=FALSE
+);
+
 -- inserts
 
 INSERT INTO Estabelecimento (descricao, rua, cep, cnpj, nome, cardapio)
-VALUES ('sempre bem animado esse lugar', 'rua dos manos', '875645', '777777', 'bar do tonhao', 'muita bebida e coisas');
+VALUES ('sempre bem animado esse lugar', 'rua dos manos', '875645', '777777', 'bar do tonhao');
 
 INSERT INTO Estabelecimento (descricao, rua, cep, cnpj, nome, cardapio)
-VALUES ('meu lugar é muito legal', 'rua dos caras', '91120564', '465789123', 'bar do zé', 'muita comida e muita coisa');
+VALUES ('meu lugar é muito legal', 'rua dos caras', '91120564', '465789123', 'bar do zé');
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (1, 1);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (2, 2);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (3, 2);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (4, 1);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (5, 1);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (6, 2);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (7, 1);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (3, 1);
+
+INSERT INTO Cardapio (dia, id_estabelecimento)
+VALUES (4, 2);
 
 
 INSERT INTO Avaliacao (nota, id_estabelecimento)
