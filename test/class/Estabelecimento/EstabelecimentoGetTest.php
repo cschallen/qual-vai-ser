a1d168767d8c7ed49b524b563eb60c5b97e4c07d
cschallenberger <c.schallenberger@hotmail.com>
2017-06-18 23:40:42 -0300

    teste pra ObterNotaMedia

--- test/class/Estabelecimento/EstabelecimentoGetTest.php
+++ test/class/Estabelecimento/EstabelecimentoGetTest.php
@@ -2,10 +2,19 @@
 
 use PHPUnit\Framework\TestCase as PHPUnit;
 
+require_once './class/Avaliacao.class.php';
 require_once './class/Estabelecimento.class.php';
 require_once './class/DonoEstabelecimento.class.php';
 
 class EstabelecimentoGetTest extends PHPUnit{
+
+    public function testObterNotaMedia() {
+        $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", '40.71727401', '-74.00898606', "99.999.999/9999-99", 1);
+        $avaliacao1 = new Avaliacao(5, 1);
+        $avaliacao2 = new Avaliacao(3, 1);
+
+        $this->assertEquals(4, $estabelecimento->ObterNotaMedia(1));
+    }
 
     public function testGetRuaTest() {
         $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", '40.71727401', '-74.00898606', "99.999.999/9999-99", 1);
@@ -31,5 +40,5 @@ class EstabelecimentoGetTest extends PHPUnit{
         $estabelecimento = new Estabelecimento("Estabelecimento Teste", "Descricao do estabelecimento", "Rua X", "91450-140", '40.71727401', '-74.00898606', "99.999.999/9999-99", 1);
         $this->assertEquals("Estabelecimento Teste", $estabelecimento->getNome());
     }
-}    
+}
 ?>
