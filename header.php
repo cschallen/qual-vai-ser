88882110cb4781f2f22555f95819af263bb821c9
Unknown <camila.prestes@yahoo.com.br>
2017-06-19 23:39:06 -0300

    Campos de busca

--- header.php
+++ header.php
@@ -12,12 +12,13 @@
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav main-nav  clear navbar-right ">
+                <ul class="nav navbar-nav main-nav  clear navbar-right ">		    
                     <li><a class="navactive color_animation" href="index.php">INÍCIO</a></li>
                     <li><a class="color_animation" href="#about">SOBRE NÓS</a></li>
                     <li><a class="color_animation" href="locais.php">LOCAIS</a></li>
                     <li><a class="color_animation" href="cadastroDono.php">CADASTRE-SE</a></li>
                     <li><a class="color_animation" href="login.php">LOGIN</a></li>
+					<li><form name="searchForm" method="post" action="busca.php"><input type="text"  name="busca" style="color:black" placeholder="Pesquisar"/> <input type="submit" style="color:black" value="Buscar"/></form></li>
                 </ul>
             </div>
         </div>
