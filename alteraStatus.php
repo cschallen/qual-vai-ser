d7d602b1d2e653ef5610c42327039a53e1c2d4dd
cschallenberger <c.schallenberger@hotmail.com>
2017-07-04 20:44:48 -0300

    @todos - inserir mais informacoes na area de admin

--- alteraStatus.php
+++ alteraStatus.php
@@ -2,20 +2,20 @@
    require_once "class/Estabelecimento.class.php";
    require_once "class/EstabelecimentoDAO.class.php";
    require_once "class/Conexao.class.php";
-    
+
     $con = new Conexao();
     $con = $con->obterConexao();
 
     $idEstabelecimento = $_GET['id'];
-    
+
     $posicao = strpos($idEstabelecimento, '/');
-    $status = substr($idEstabelecimento, $posicao+1); 
-    
+    $status = substr($idEstabelecimento, $posicao+1);
+
     $idEstab = strstr($idEstabelecimento, "/".$status, true);
-    
-    
+
+
     $comando = "UPDATE estabelecimento SET status = '$status' WHERE id_estabelecimento = '$idEstab'";
-    return $con->exec($comando);
-	$redireciona = "areaAdmin.php";
-   	header("location:$redireciona");
+    $con->exec($comando);
+
+   	header("location: areaAdmin.php");
 ?>
